77e0245424217beb8bc8d8de1704b79b9effa90d
rodriver1992 <rodriver1992@gmail.com>
2024-06-27 23:12:28 +0000

    added input's validations in both forms

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   const $id = $("#identifier");
   const $name = $("#name");
+  
   if ($("#product").length) {
     const $description = $("#description");
     const $price = $("#price");
@@ -14,9 +15,43 @@ $(document).ready(function () {
     });
   }
 
+  $('#bg-disable').click(() => {
+    destroyLi();
+    $('#bg-disable').addClass('inactive');
+  })
+
   function trimElements(elements) {
+    let emptyElements = [];
     elements.forEach((element) => {
       element.val(element.val().trim());
+      if (element.val() === "") {
+        emptyElements.push(element);
+      }
+    });
+
+    if (emptyElements.length) {
+      showAlertMsg(emptyElements);
+    }
+  }
+
+  function showAlertMsg(elements) {
+    const $ul = $('#alert-msg ul');
+
+    $('#bg-disable').removeClass('inactive');
+    
+    for(var element of elements) {
+      const $li = $('<li>');
+
+      $li.text(element.attr('placeholder'));
+      $ul.append($li);
+    }
+  }
+
+  function destroyLi() {
+    const liElements = Array.from($('#alert-msg ul').children());
+
+    liElements.forEach((element) => {
+      element.remove();
     });
   }
 });
