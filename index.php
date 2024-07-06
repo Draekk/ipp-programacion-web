f1e152b5a4654c094eba858d005dd91fb757563e
draekkdev <rodriver1992@gmail.com>
2024-07-06 13:04:40 -0400

    testing queries

--- index.php
+++ index.php
@@ -8,32 +8,57 @@
 <body>
   <?php
 
-  //include PDO
-  include './class/database.php';
+  // //include PDO
+  // include './class/database.php';
 
-  //Instanciar la clase Database
-  $database = new Database('mysql', '127.0.0.1', 'custom_user', '1234', 'db_miproyecto');
+  // //Instanciar la clase Database
+  // $database = new Database('mysql', '127.0.0.1', 'custom_user', '1234', 'db_miproyecto');
 
-  //Obtener la conexion
-  $conn = $database->getConn();
+  // //Obtener la conexion
+  // $conn = $database->getConn();
 
-  if($conn) {
-    echo "<h1>Conectado</h1>";
+  // if($conn) {
+  //   echo "<h1>Conectado</h1>";
 
-    $select = $conn->query('SELECT * FROM categorias');
-    $result = $select->fetchAll(PDO::FETCH_ASSOC);
+    // $select = $conn->query('SELECT * FROM categorias');
+    // $result = $select->fetchAll(PDO::FETCH_ASSOC);
 
 
-    echo "<ul>";
-    foreach($result as $row) {
-      echo "<li>".$row['identificador']."</li>";
-      echo "<li>".$row['nombre']."</li>";
-    }
-    echo "</ul>";
+    // echo "<ul>";
+    // foreach($result as $row) {
+    //   echo "<li>".$row['identificador']."</li>";
+    //   echo "<li>".$row['nombre']."</li>";
+    // }
+    // echo "</ul>";
 
-  } else {
-    echo "<h1>Desconectado</h1>";
-  }
+    // $insert = $conn->prepare("INSERT INTO categorias (identificador, nombre) VALUES (5432, 'harinas')");
+
+    // if($insert->execute()) {
+    //   echo "<h1>Insertado</h1>";
+    // } else {
+    //   echo "<h1>No insertado</h1>";
+    // }
+
+    // $delete = $conn->prepare("DELETE FROM categorias WHERE identificador = 5432");
+
+    // if($delete->execute()) {
+    //   echo "<h1>Eliminado</h1>";
+    // } else {
+    //   echo "<h1>No eliminado</h1>";
+    // }
+
+    // $update = $conn->prepare("UPDATE categorias SET identificador = 1 WHERE identificador = 1234");
+
+    // if($update->execute()) {
+    //   echo "<h1>Actualizado</h1>";
+    // } else {
+    //   echo "<h1>No actualizado</h1>";
+    // }
+
+
+  // } else {
+  //   echo "<h1>Desconectado</h1>";
+  // }
   
   // $conn = new PDO('mysql:db_miproyecto;host=127.0.0.1', 'root', 'DraekkDev6592');
   
