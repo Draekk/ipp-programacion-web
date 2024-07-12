ab3c18c404d2f69152a7ed7bf71f26542b3921cc
draekkdev <rodriver1992@gmail.com>
2024-07-12 11:28:15 -0400

    added update, delete, getById functions to database.php

--- class/database.php
+++ class/database.php
@@ -81,6 +81,95 @@ class Database {
     }
   }
 
+  /**
+  * Actualiza un registro en la base de datos según los datos proporcionados.
+  *
+  * Este método acepta un objeto de datos y actualiza el registro correspondiente en la base de datos,
+  * dependiendo de su tipo. Si el objeto es de tipo 'Categoria', los datos se actualizarán en la tabla 'categorias'.
+  * Si el objeto es de tipo 'Producto', los datos se actualizarán en la tabla 'productos'.
+  *
+  * @param mixed $data El objeto de datos para actualizar en la base de datos.
+  *
+  * @throws Exception Si se produce un error al ejecutar la consulta de actualización.
+  */
+  public function uptate($data) {
+    try {
+      if($data instanceof Categoria) {
+        $query = $this->conn->prepare("UPDATE categorias SET nombre = :name WHERE identificador = :id");
+        $query->bindParam(':id', $data->getId());
+        $query->bindParam(':name', $data->getName());
+        $query->execute();
+      } else {
+        $query = $this->conn->prepare("UPDATE productos SET nombre = :name, imagen = :image, descripcion = :description, precio = :price, categoria = :category WHERE identificador = :id");
+        $query->bindParam(':id', $data->getId());
+        $query->bindParam(':name', $data->getName());
+        $query->bindParam(':image', $data->getImage(), PDO::PARAM_LOB);
+        $query->bindParam(':description', $data->getDescription());
+        $query->bindParam(':price', $data->getPrice());
+        $query->bindParam(':category', $data->getCategory());
+      }
+    } catch(Exception $ex) {
+      throw new Exception($ex->getMessage());
+    }
+  }
+
+  /**
+  * Elimina un registro de la base de datos según los datos proporcionados.
+  *
+  * Este método acepta un objeto de datos y elimina el registro correspondiente de la base de datos,
+  * dependiendo de su tipo. Si el objeto es de tipo 'Categoria', los datos se eliminarán de la tabla 'categorias'.
+  * Si el objeto es de tipo 'Producto', los datos se eliminarán de la tabla 'productos'.
+  *
+  * @param mixed $data El objeto de datos para eliminar de la base de datos.
+  *
+  * @throws Exception Si se produce un error al ejecutar la consulta de eliminación.
+  */
+  public function delete($data) {
+    try {
+      if($data instanceof Categoria) {
+        $query = $this->conn->prepare("DELETE FROM categorias WHERE identificador = :id");
+        $query->bindParam(':id', $data->getId());
+        $query->execute();
+      } else {
+        $query = $this->conn->prepare("DELETE FROM productos WHERE identificador = :id");
+        $query->bindParam(':id', $data->getId());
+        $query->execute();
+      }
+    } catch(Exception $ex) {
+      throw new Exception($ex->getMessage());
+    }
+  }
+
+  /**
+  * Recupera un registro de la base de datos según la tabla y el ID proporcionados.
+  *
+  * Esta función ejecuta una sentencia preparada para seleccionar un registro de la tabla especificada,
+  * basándose en el ID proporcionado. Si la tabla es 'categorias', recupera todas las columnas de la tabla.
+  * Si la tabla es 'productos', recupera columnas específicas junto con el nombre de la categoría correspondiente.
+  *
+  * @param string $table El nombre de la tabla de la cual recuperar el registro.
+  * @param mixed $id El identificador único del registro a recuperar.
+  *
+  * @return array|null Un array asociativo que contiene el registro recuperado, o null si no se encuentra ningún registro.
+  *
+  * @throws Exception Si se produce un error durante la ejecución de la consulta de base de datos.
+  */
+  public function getById($table, $id) {
+    try {
+      if($table === 'categorias') {
+        $query = $this->conn->prepare("SELECT * FROM ". $table. " WHERE identificador = :id");
+        $query->bindParam(':id', $id);
+      } else {
+        $query = $this->conn->prepare("SELECT productos.identificador, productos.nombre, productos.imagen, productos.descripcion, productos.precio, categorias.nombre as nombre_categoria FROM ". $table. " JOIN categorias ON productos.categoria = categorias.identificador WHERE productos.identificador = :id");
+        $query->bindParam(':id', $id);
+      }
+      $query->execute();
+      return $query->fetch(PDO::FETCH_ASSOC);
+    } catch (Exception $ex) {
+      throw new Exception($ex->getMessage());
+    }
+  }
+
 
 }
 
