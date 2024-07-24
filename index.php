85ca45b71bf1a8059450e7f5b405daf0f3a0f088
draekkdev <rodriver1992@gmail.com>
2024-07-23 23:42:06 -0400

    added some comments

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
-
+//Acceder a la clase Autoload
 require_once __DIR__ . "/class/autoload.php";
 
 $autoload = new Autoload();
 $database = $autoload->getDatabase();
 
 try {
+  //Obtener la lista de productos desde la base de datos
   session_start();
   $products = $database->findAll('productos');
 } catch (Exception $ex) {
   throw new Exception($ex->getMessage());
 }
 
+//Mostrar la lista de productos en la vista home.html
 include "./views/home.html";
 
 ?>
